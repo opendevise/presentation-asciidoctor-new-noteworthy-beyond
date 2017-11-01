ba1e8da4b5bf1ccdf0ce4697e357978a542495f7
Dan Allen <dan.j.allen@gmail.com>
2017-11-01 17:59:43 -0600

    force transform scaling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,16 @@
 var bespoke = require('bespoke'),
-  bullets = require('bespoke-bullets'),
-  classes = require('bespoke-classes'),
-  extern = require('bespoke-extern'),
-  hash = require('bespoke-hash'),
-  multimedia = require('bespoke-multimedia'),
-  nav = require('bespoke-nav'),
-  overview = require('bespoke-overview'),
-  scale = require('bespoke-scale');
+    bullets = require('bespoke-bullets'),
+    classes = require('bespoke-classes'),
+    extern = require('bespoke-extern'),
+    hash = require('bespoke-hash'),
+    multimedia = require('bespoke-multimedia'),
+    nav = require('bespoke-nav'),
+    overview = require('bespoke-overview'),
+    scale = require('bespoke-scale')
 
 bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   classes(),
-  scale(),
+  scale('transform'),
   nav(),
   overview(),
   bullets('.build, .build-items > *:not(.build-items)'),
